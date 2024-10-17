ec4453bf776ca5851e0f560fc7a614e03953b631
MarlonHDC <marlon_hdc@hotmail.com>
2024-10-17 13:03:18 -0300

    entrada de dados update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const prompt = require('prompt-sync')()
 
 nomeDoHeroi = prompt("Digite o nome do seu herói: ")
-qtdExperiencia = prompt("Digite o nome do seu herói: ")
+qtdExperiencia = prompt("Quantidade de experiência do seu herói: ")
 
 
 
